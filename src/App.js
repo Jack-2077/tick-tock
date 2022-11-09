1e75703ea26c66ce344c457cd8db204010c5c8e6
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-11-08 21:56:32 -0800

    feat: display time every second

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { useState, useEffect } from 'react';
+
 function App() {
-  return <h1>Hello World</h1>;
+  const [dateState, setDateState] = useState(new Date());
+
+  useEffect(() => {
+    setInterval(() => setDateState(new Date()), 1000);
+  }, []);
+
+  const toggleFullScreen = () => {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+    } else if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={toggleFullScreen}>FULLSCREEN</button>
+
+      <p>
+        {dateState.toLocaleString('en-US', {
+          hour: 'numeric',
+          minute: 'numeric',
+          second: 'numeric',
+          hour12: true,
+        })}
+      </p>
+    </div>
+  );
 }
 
 export default App;
