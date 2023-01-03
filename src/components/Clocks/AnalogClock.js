32bb070b70824c4b7bb74d232b730cd84d15cfad
Jack <78120697+Jack-2077@users.noreply.github.com>
2023-01-02 20:13:01 -0800

    style: analog clock with extended styles

--- src/components/Clocks/AnalogClock.js
+++ src/components/Clocks/AnalogClock.js
@@ -1,15 +1,18 @@
 import React, { memo } from 'react';
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { StyledAnalogClock } from '../styles/AnalogClockStyles';
+import { StyledAnalogClock, StyledClock } from '../styles/AnalogClockStyles';
 
 const StyledClockHand = styled.div`
   transform: rotateZ(${(props) => props.angle}deg);
 `;
+const StyledMinuteBlock = styled.span`
+  transform: translate(-50%, -50%) rotate(${(props) => props.angle}deg);
+`;
 
 const MemoizedClockHand = memo(({ time, type, angleToRotate }) => {
   return (
-    <StyledClockHand className={type + '_hand'} angle={time * angleToRotate} />
+    <StyledClockHand className={type + '-hand'} angle={time * angleToRotate} />
   );
 });
 
@@ -19,21 +22,19 @@ const MemoizedClockHand = memo(({ time, type, angleToRotate }) => {
 
 const MinuteSpan = ({ angle }) => {
   const arr = [];
-  for (let i = angle, j = 0; j < 5; j++, i += 6) {
+  for (let i = angle, j = 0; j < 4; j++, i += 6) {
     arr.push(i);
   }
 
+  const isHalfway = [90, 180].includes(angle + 24);
   return (
     <>
       {arr.map((angleToBend) => (
-        <span
-          className='minutes'
-          key={angleToBend}
-          style={{
-            transform: `translate(-50%, -50%) rotate(${angleToBend}deg)`,
-          }}
-        />
+        <StyledMinuteBlock key={angleToBend} angle={angleToBend} />
       ))}
+      {!isHalfway && (
+        <StyledMinuteBlock className='hour-block' angle={angle + 24} />
+      )}
     </>
   );
 };
@@ -50,7 +51,59 @@ export default function AnalogClock({ primaryColor }) {
 
   return (
     <StyledAnalogClock color={primaryColor}>
-      <div className='clock'>
+      <div className='circle' />
+      <MemoizedClockHand
+        time={time.getHours()}
+        angleToRotate={30}
+        type='hour'
+      />
+      <MemoizedClockHand
+        time={time.getMinutes()}
+        angleToRotate={6}
+        type='minute'
+      />
+      <MemoizedClockHand
+        time={time.getSeconds()}
+        angleToRotate={6}
+        type='second'
+      />
+      <ul>
+        <li className='twelve'>
+          <i>12</i>
+        </li>
+        <li className='three'>
+          <i>3</i>
+        </li>
+        <li className='six'>
+          <i>6</i>
+        </li>
+        <li className='nine'>
+          <i>9</i>
+        </li>
+      </ul>
+      <MinuteSpan angle={6} />
+      <MinuteSpan angle={36} />
+      <MinuteSpan angle={66} />
+      <MinuteSpan angle={96} />
+      <MinuteSpan angle={126} />
+      <MinuteSpan angle={156} />
+    </StyledAnalogClock>
+  );
+}
+
+export function NewClock() {
+  const [time, setTime] = useState(() => new Date('Dec 25 2022 10:10:35'));
+  // useEffect(() => {
+  //   const id = setInterval(() => {
+  //     setTime(new Date());
+  //   }, 1000);
+  //   return () => clearInterval(id);
+  // }, []);
+  return (
+    <StyledClock color='red'>
+      <section className='border-clock' />
+      <section className='clock'>
+        <div className='circle' />
         <MemoizedClockHand
           time={time.getHours()}
           angleToRotate={30}
@@ -59,38 +112,37 @@ export default function AnalogClock({ primaryColor }) {
         <MemoizedClockHand
           time={time.getMinutes()}
           angleToRotate={6}
-          type='min'
+          type='minute'
         />
         <MemoizedClockHand
           time={time.getSeconds()}
           angleToRotate={6}
-          type='sec'
+          type='second'
         />
-        <span className='twelve'>12</span>
+        {/* <div className='minute-hand' />
+        <div className='hour-hand' />
+        <div className='second-hand' /> */}
+        <ul>
+          <li className='twelve'>
+            <i>12</i>
+          </li>
+          <li className='three'>
+            <i>3</i>
+          </li>
+          <li className='six'>
+            <i>6</i>
+          </li>
+          <li className='nine'>
+            <i>9</i>
+          </li>
+        </ul>
         <MinuteSpan angle={6} />
-        <span className='one' />
         <MinuteSpan angle={36} />
-        <span className='two' />
         <MinuteSpan angle={66} />
-        <span className='three'>3</span>
         <MinuteSpan angle={96} />
-        <span className='four' />
         <MinuteSpan angle={126} />
-        <span className='five' />
         <MinuteSpan angle={156} />
-        <span className='six'>6</span>
-        <MinuteSpan angle={186} />
-        <span className='seven' />
-        <MinuteSpan angle={216} />
-        <span className='eight' />
-        <MinuteSpan angle={246} />
-        <span className='nine'>9</span>
-        <MinuteSpan angle={276} />
-        <span className='ten' />
-        <MinuteSpan angle={306} />
-        <span className='eleven' />
-        <MinuteSpan angle={336} />
-      </div>
-    </StyledAnalogClock>
+      </section>
+    </StyledClock>
   );
 }
--- src/components/styles/AnalogClockStyles.js
+++ src/components/styles/AnalogClockStyles.js
@@ -1,166 +1,169 @@
 import styled from 'styled-components';
 
 export const StyledAnalogClock = styled.div`
-  .clock {
-    width: 300px;
-    height: 300px;
-    border-radius: 50%;
+  font-family: 'Roboto', sans-serif;
+  width: 272px;
+  height: 272px;
+  background-color: #000;
+  border-radius: 100%;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  box-shadow: 0 2px 30px ${(props) => props.color};
+  border: 3.5px solid #282828;
+
+  ul {
+    list-style: none;
+    height: 38%;
+    width: 0;
     position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    border-style: solid;
-    border-width: 5px;
-    box-shadow: -15px 10px 150px -9px #7a66f7;
-    /* box-shadow: 0px 10px 35px -11px ${(props) => props.color}; */
-    /* box-shadow: 0 2px 30px ${(props) => props.color}; */
-    font-size: 24px;
-    color: #fff;
-    text-align: center;
-    /* background: ${(props) => props.color}; */
-  }
-
-  .clock::after {
-    background: #50cf9f;
-    content: '';
-    width: 5px;
-    height: 5px;
-    border-radius: 50%;
-    position: absolute;
-    z-index: 2;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    border: 2px solid #50cf9f;
-  }
-
-  .hour_hand {
-    position: absolute;
-    width: 6px;
-    height: 60px;
-    background: #aaffdf;
-    top: 30%;
-    left: 49%;
-    transform-origin: bottom;
-    border-radius: 16px;
-  }
-
-  .min_hand {
-    position: absolute;
-    width: 4px;
-    height: 85px;
-    background: #fff;
-    top: 22%;
-    left: 48.9%;
-    transform-origin: bottom;
-    border-radius: 16px;
-  }
-
-  .sec_hand {
-    position: absolute;
-    width: 1.5px;
-    height: 115px;
-    background: #50cf9f;
-    top: 12%;
-    left: 49.8%;
-    transform-origin: bottom;
-    border-radius: 16px;
-  }
-
-  .clock span {
-    position: absolute;
-    font-weight: 700;
-  }
-
-  .one,
-  .two,
-  .four,
-  .five,
-  .seven,
-  .eight,
-  .ten,
-  .eleven {
-    display: block;
-    height: 10px;
-    width: 4px;
-    border-radius: 0 0 100px 100px;
-    background-color: #fff;
-  }
-
-  .minutes {
-    display: block;
-    width: 1px;
-    height: 5px;
-
-    top: 50%;
+    bottom: 50%;
     left: 50%;
   }
 
-  .twelve {
-    top: 10px;
-    left: 46%;
-  }
+  ul li {
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    transform-origin: 50% 100%;
 
-  .one {
-    top: 10%;
-    right: 26%;
-    transform: translate(-50%, -50%) rotate(30deg);
-  }
-
-  .eleven {
-    top: 10%;
-    left: 26%;
-    transform: translate(-50%, -50%) rotate(330deg);
-  }
-
-  .two {
-    top: 25%;
-    right: 10%;
-    transform: translate(-50%, -50%) rotate(60deg);
+    i {
+      color: #fff;
+      position: absolute;
+      top: 0;
+      left: 50%;
+      transform: translate(-50%, -90%);
+    }
   }
 
   .three {
-    right: 10px;
-    top: 46%;
-  }
-
-  .four {
-    right: 30px;
-    top: 67%;
-    transform: translate(-50%, -50%) rotate(120deg);
-  }
-
-  .five {
-    right: 78px;
-    top: 80%;
-    transform: translate(-50%, -50%) rotate(150deg);
+    transform: rotate(90deg);
+    i {
+      transform: translate(120%, -180%) rotate(-90deg);
+    }
   }
 
   .six {
-    bottom: 10px;
-    left: 50%;
-  }
-
-  .seven {
-    left: 80px;
-    top: 82%;
-    transform: translate(-50%, -50%) rotate(210deg);
-  }
-
-  .eight {
-    left: 30px;
-    top: 67%;
-    transform: translate(-50%, -50%) rotate(240deg);
+    transform: rotate(180deg);
+    i {
+      transform: translate(-50%, -270%) rotate(-180deg);
+    }
   }
 
   .nine {
-    left: 10px;
-    top: 46%;
+    transform: rotate(270deg);
+    i {
+      transform: translate(-220%, -180%) rotate(-270deg);
+    }
   }
 
-  .ten {
-    top: 25%;
-    left: 10%;
-    transform: translate(-50%, -50%) rotate(300deg);
+  span {
+    display: block;
+    width: 1px;
+    height: 95%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+
+    &::before,
+    &::after {
+      content: '';
+      background-color: #a0a1a4;
+      position: absolute;
+      width: 100%;
+      height: 2px;
+      bottom: 0;
+      left: 0;
+    }
+
+    &::after {
+      top: 0;
+    }
+  }
+
+  .hour-block {
+    &::before,
+    &::after {
+      background-color: red !important;
+      height: 8px !important;
+      width: 2px !important;
+      border-radius: 0 0 100px 100px;
+    }
+
+    &::after {
+      border-radius: 100px 100px 0px 0px;
+    }
+  }
+
+  .circle {
+    width: 9px;
+    height: 9px;
+    background-color: #fdfaf7;
+    border-radius: 100%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 6;
+  }
+
+  .hour-hand,
+  .minute-hand {
+    width: 3px;
+    height: 1px;
+    position: absolute;
+    left: 49.5%;
+    top: 50%;
+    transform-origin: 50% 0;
+    z-index: 5;
+
+    &::before {
+      width: 3px;
+      height: 80px;
+      background-color: #fff;
+      border-radius: 3px;
+
+      content: '';
+      position: absolute;
+      bottom: 49.5%;
+      left: 50%;
+      transform-origin: 50% 0;
+      transform: translateX(-50%);
+    }
+
+    &::after {
+      content: '';
+      display: block;
+      position: absolute;
+      background: #fff;
+      border-radius: 3px;
+
+      width: 3px;
+      height: 10px;
+      top: 3px;
+    }
+  }
+
+  .minute-hand {
+    opacity: 0.9;
+    left: 49.5%;
+    z-index: 4;
+    &::before {
+      height: 105px;
+    }
+  }
+
+  .second-hand {
+    position: absolute;
+    border-radius: 100%;
+    width: 4px;
+    height: 4px;
+    background: ${(props) => props.color};
+    top: 7.1%;
+    left: 51%;
+    transform-origin: 0 15vh;
+    border-radius: 16px;
   }
 `;
